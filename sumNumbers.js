0b48be2aba24ae8be9b686d4a3fe13b5f7e1b4b4
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-20 19:46:02 -0400

    Completed Sum the Numbers exercise

---
+++ sumNumbers.js
@@ -0,0 +1,10 @@
+function sumNumbers(arr) {
+    sum = 0;
+    arr.forEach(num => {
+        sum += num;
+    });
+    return sum
+}
+
+console.log(sumNumbers([1, 4, 8]));
+console.log(sumNumbers([5, 5, 10]));
