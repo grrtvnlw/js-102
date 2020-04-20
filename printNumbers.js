75e6088b296a009b7226f56c2eb7eb0b6568809f
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-20 14:12:00 -0400

    Completed Print Numbers and Bonus

---
+++ printNumbers.js
@@ -0,0 +1,18 @@
+// While Loop version
+function printNumbers(a, b) {
+    while (a <= b) {
+        console.log(a);
+        a++;
+    }
+}
+
+printNumbers(1, 10);
+
+// For Loop version
+function printNums(a, b) {
+    for (let i = a; i <= b; i++) {
+        console.log(i);
+    }
+}
+
+printNums(1, 10);
