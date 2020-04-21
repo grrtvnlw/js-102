abf4ab8fe1824b7880302a8a45a0746130b0c4b0
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-21 08:01:27 -0400

    Refactored Matrix Multiply - exercise completed

--- matrixMultiply.js
+++ matrixMultiply.js
@@ -1,6 +1,5 @@
 function matrixMultiply(arr) {
     let returnArray = [[], []]
-    console.log(arr[0][1][0])
     let firstResult = arr[0][0][0] * arr[1][0][0] + arr[0][0][1] * arr[1][1][0];
     let secondResult = arr[0][0][0] * arr[1][0][1] + arr[0][0][1] * arr[1][1][1];
     let thirdResult = arr[0][1][0] * arr[1][0][0] + arr[0][1][1] * arr[1][1][0];
@@ -9,8 +8,7 @@ function matrixMultiply(arr) {
     returnArray[0].push(secondResult);
     returnArray[1].push(thirdResult);
     returnArray[1].push(fourthResult);
-    console.log(returnArray)
-
+    return returnArray;
 }
 
-matrixMultiply([[[2, 4], [3, 4]], [[5, 2], [3, 1]]]);
+console.log(matrixMultiply([[[2, 4], [3, 4]], [[5, 2], [3, 1]]]));
